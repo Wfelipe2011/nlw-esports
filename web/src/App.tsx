49c5dcbc1a84dbfd5d31a48b775de4b40725bc7a
Wilson Felipe <wfelipe2011@gmail.com>
2022-09-13 19:15:32 -0300

    init home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import "./assets/css/main.css";
 
+import logoImg from "./assets/images/logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-     init
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 font-Inter">
+      <img src={logoImg} />
+
+      <h1 className="text-5xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+        </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
